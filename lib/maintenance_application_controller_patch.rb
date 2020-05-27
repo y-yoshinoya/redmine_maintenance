ac3a22088db2ad72e187553bc7d67cfc7aae0b5e
y-yoshinoya <y-yoshinoya@ase.co.jp>
2020-05-27 14:46:14 +0900

    Compatible with Redmine 4.0+

--- lib/maintenance_application_controller_patch.rb
+++ lib/maintenance_application_controller_patch.rb
@@ -1,11 +1,11 @@
 require_dependency 'application_controller'
 
 class ApplicationController < ActionController::Base
-  before_filter :maintenance
+  before_action :maintenance
 
-  # メンテナンスy画面を表示する
+  # メンテナンス画面を表示する
   def maintenance
-    if '1' == Setting.plugin_redmine_maintenance['maintenance'] 
+    if '1' == Setting.plugin_redmine_maintenance['maintenance']
       # 管理者以外はメンテナンス画面を表示する
       # ログイン画面の表示は可能にする
       if !User.current.admin? && !('account' == controller_name && 'login' == action_name)
